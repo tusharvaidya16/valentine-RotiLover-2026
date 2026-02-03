ebcda63a6f70f39338fc019d1351afdf64b77b11
Tushar Vaidya <30749153+tusharvaidya16@users.noreply.github.com>
2026-02-03 14:24:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹 But most important, Can you be my forever fight girl?🫰🏻", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹 But most important, Can you be my forever fight girl?🫰🏻", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
